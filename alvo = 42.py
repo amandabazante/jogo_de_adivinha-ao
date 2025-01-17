b6663de79880105fd4e4b5ce4fb3bc07a523a61d
amandabazante <amandabazante@outlook.com>
2025-01-16 21:33:24 -0300

    alterações no protótipo

--- alvo = 42.py
+++ alvo = 42.py
@@ -1,13 +1,26 @@
 alvo = 42
+quantidade_tentativas = 5
+jogador_acertou = False
 
-try:
 
-  chute = int(input("Digite um número inteiro entre 0 e 100 "))
+while quantidade_tentativas > 0 and not jogador_acertou:
 
-  if chute == alvo:
-      print("Parabéns! Você acertou!")
-  else:
-      print("Que pena! O número era " + str(alvo))
+    try:
 
-except:
-    print("O valor não era um número inteiro")
+        chute = int(input("Digite um número inteiro entre 0 e 100 "))
+
+        if chute == alvo:
+            print("Parabéns! Você acertou!")
+            jogador_acertou = True
+        elif chute > alvo:
+            print("Errou, o número é menor")
+            quantidade_tentativas -= 1
+        else:
+            print("Errou, o número é maior")
+            quantidade_tentativas = quantidade_tentativas - 1
+
+    except:
+        print("O valor não era um número inteiro")
+
+if not jogador_acertou:
+    print("Que pena, o número era ")
